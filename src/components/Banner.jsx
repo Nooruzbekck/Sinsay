8c59e2dc468820c2f5357166eb9b5643626bfad5
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-10-10 02:30:56 +0300

    finished-banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Restor from "../icons/Restor.png";
+import styled from "styled-components";
+const Banner = () => {
+  return (
+    <DivStyle>
+      <BannerStyle src={Restor} alt="" />
+      <TextStyle>View all products</TextStyle>
+    </DivStyle>
+  );
+};
+
+export default Banner;
+const DivStyle = styled.div`
+  margin: 10px;
+  height: 100%;
+`;
+const BannerStyle = styled.img`
+  width: 100%;
+  height: 20%;
+`;
+const TextStyle = styled.h2`
+  font-family: Montserrat;
+  font-size: 61px;
+  font-weight: 500;
+  line-height: 73.2px;
+  align-items: center;
+  color: #000000;
+  position: relative;
+  bottom: 350px;
+  left: 100px;
+  width: 45vh;
+  height: 0%;
+  display: block;
+  overflow: hidden;
+  width: 30%px;
+`;
