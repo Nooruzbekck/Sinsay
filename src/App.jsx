97837f918e1e36b2e84e068056a940757ddeeb57
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-10-10 20:54:51 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { Icons } from "./components/assets";
+import Header from "./components/Header/Header";
+
 export function App() {
-  return <div></div>;
+  return <div>
+    <Header>
+      <Icons.Burgermenu />
+    </Header>
+  </div>;
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <StyleHeader>
-      <img src={Icons.Burgermenu} alt="" />
+      <img src={Icons.Burgermenu} alt="Burger" />
       <h2>SINSAY</h2>
       <ChildDiv>
         <img src={Icons.Search} alt="Search" />
