687d259b1dfd8d563f27e9eb05ed8d65ab9d15e3
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-10-10 13:13:15 +0300

    finished-header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icons from "../assets/index.js";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={Icons.Korzina} alt="" />
+      <h1>fghjkl</h1>
+    </div>
+  )
+}
+
+export default Header
