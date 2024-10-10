d1433a4fbc8d256c71da58bcbf0365b72a525e27
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-10-10 15:58:51 +0300

    finished-header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Icons } from '../assets/index'
-import styled from 'styled-components'
+import React from "react";
+import { Icons } from "../assets/index";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-      <StyleHeader>
-       <img src={Icons.Burgermenu} alt="" />
-       <h2>SINSAY</h2>
-       <ChildDiv>
-       <img src={Icons.Search} alt="Search" />
-       <img src={Icons.People} alt="People" />
-       <img src={Icons.Heart} alt="Heart" />
-       <img src={Icons.Bag} alt="Korzina" />
-       </ChildDiv>
-      </StyleHeader>
-  )
-}
+    <StyleHeader>
+      <img src={Icons.Burgermenu} alt="" />
+      <h2>SINSAY</h2>
+      <ChildDiv>
+        <img src={Icons.Search} alt="Search" />
+        <img src={Icons.People} alt="People" />
+        <img src={Icons.Heart} alt="Heart" />
+        <img src={Icons.Bag} alt="Korzina" />
+      </ChildDiv>
+    </StyleHeader>
+  );
+};
 
-export default Header
+export default Header;
 const StyleHeader = styled.header`
   display: flex;
   justify-content: space-between;
   border-bottom: 2px solid black;
-  padding-left: 40px ;
+  padding-left: 40px;
   padding-right: 100px;
 `;
 const ChildDiv = styled.div`
-display: flex;
-padding-top: 13px;
-width: 80px;
-height: 30px;
-gap: 15px;
+  display: flex;
+  padding-top: 13px;
+  width: 80px;
+  height: 30px;
+  gap: 15px;
 `;
