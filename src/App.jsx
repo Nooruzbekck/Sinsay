30aa418388c8c4f8487fbda1be0a50d06ffaa093
Azamat <azamalik.011124@gmail.com>
2024-10-11 14:26:11 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import Banner from "./components/Banner";
+import { MainLayout } from "./layout/MainLayout";
+
 export function App() {
-  return <div></div>;
+  return (
+    <div>
+    <Banner/>
+    <h1>Face care</h1>
+      <MainLayout />
+    </div>
+  );
 }
