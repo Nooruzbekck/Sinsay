f07cc5049595dab7c9567e2a64904b6228117592
Azamat <azamalik.011124@gmail.com>
2024-10-11 17:13:41 +0300

    slaider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import Banner from "./components/Banner";
-import { MainLayout } from "./layout/MainLayout";
 
 export function App() {
   return (
     <div>
     <Banner/>
-    <h1>Face care</h1>
-      <MainLayout />
     </div>
   );
 }
