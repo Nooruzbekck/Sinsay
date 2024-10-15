366c2e1f287142916fe07b9df92c69e30e012296
Azamat <azamalik.011124@gmail.com>
2024-10-16 00:22:48 +0300

    save

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,41 @@
-import React from "react";
-import Restor from "../icons/Restor.png";
+import React, { useState, useEffect } from "react";
+import Restor1 from "../icons/Restor.png";
+import Restor2 from "../assets/crem1.webp";
+import Restor3 from "../assets/crem.avif";
 import styled from "styled-components";
+
 const Banner = () => {
+  const banners = [Restor1, Restor2, Restor3];
+
+  const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentBannerIndex((prevIndex) => (prevIndex + 1) % banners.length);
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [banners.length]);
+
   return (
     <DivStyle>
-      <BannerStyle src={Restor} alt="" />
+      <BannerStyle src={banners[currentBannerIndex]} alt="Banner" />
       <TextStyle>View all products</TextStyle>
     </DivStyle>
   );
 };
 
 export default Banner;
+
 const DivStyle = styled.div`
   margin: 10px;
 `;
+
 const BannerStyle = styled.img`
   width: 100%;
   height: 100vh;
 `;
+
 const TextStyle = styled.h2`
   font-family: Montserrat;
   font-size: 61px;
@@ -32,5 +50,5 @@ const TextStyle = styled.h2`
   height: 0%;
   display: block;
   overflow: hidden;
-  width: 30%px;
+  width: 30%;
 `;
